61f12d9545e1eee5a7b611c1f7b3c1b2a571a69b
Erik Malmqvist Jakobsen <erja@itu.dk>
2023-11-21 15:47:24 +0100

    final

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"math/rand"
 	"net"
 	"os"
@@ -23,12 +24,13 @@ type msg struct {
 
 type peer struct {
 	message.UnimplementedServiceServer
-	id      int32
-	mutex   sync.Mutex
-	replies map[int32]bool
-	held    chan bool
-	state   string
-	lamport int32
+	id       int32
+	mutex    sync.Mutex
+	replies  map[int32]bool
+	held     chan bool
+	state    string
+	lamport  int32
+	last_Req int32
 	// a "queue"
 	queue []msg
 	// Update on this channel, when messages should be dequeued
@@ -45,14 +47,15 @@ func main() {
 	defer cancel()
 
 	p := &peer{
-		id:      ownPort,
-		clients: make(map[int32]message.ServiceClient),
-		replies: make(map[int32]bool),
-		held:    make(chan bool),
-		ctx:     ctx,
-		state:   "RELEASED",
-		lamport: 0,
-		reply:   make(chan bool),
+		id:       ownPort,
+		clients:  make(map[int32]message.ServiceClient),
+		replies:  make(map[int32]bool),
+		held:     make(chan bool),
+		ctx:      ctx,
+		state:    "RELEASED",
+		lamport:  0,
+		last_Req: 0,
+		reply:    make(chan bool),
 	}
 
 	for peer := range p.replies {
@@ -149,17 +152,18 @@ func main() {
 		p.state = "WANTED"
 		p.mutex.Unlock()
 		p.enter()
-		// wait for state to be held
+		// wait for state to be held, write to file
 		<-p.held
 		p.lamport++
+
 		log.Printf("(%v, %v) Internal | Entered critical section\n", p.id, p.lamport)
-		writeToFile(cs, p.id, p.lamport, "enters critical section.")
+		accessRessource(cs, p.id, p.lamport, "enters critical section.")
 		var random = rand.Intn(5)
 		time.Sleep(time.Duration(random) * time.Second)
 		p.lamport++
 		log.Printf("(%v, %v) Internal | Leaving critical section\n", p.id, p.lamport)
-		writeToFile(cs, p.id, p.lamport, "leaves critical section.")
-		p.reply <- true
+		accessRessource(cs, p.id, p.lamport, "leaves critical section.")
+		p.reply <- true //exit()
 		time.Sleep(100 * time.Millisecond)
 	}
 
@@ -167,23 +171,23 @@ func main() {
 
 func (p *peer) Request(ctx context.Context, req *message.Info) (*message.Empty, error) {
 	p.mutex.Lock()
+	p.lamport = max(req.Lamport, p.lamport)
+	if p.state == "HELD" || (p.state == "WANTED" && ((p.last_Req < req.Lamport) || (p.last_Req == req.Lamport && p.id < req.Id))) {
 
-	if p.state == "HELD" || (p.state == "WANTED" && ((p.lamport < req.Lamport) || (p.lamport == req.Lamport && p.id < req.Id))) {
-		p.lamport = max(req.Lamport, p.lamport) + 1
 		log.Printf("(%v, %v) Recv | queueing request from %v\n", p.id, p.lamport, req.Id)
-
+		//p.lamport++
 		p.queue = append(p.queue, msg{id: req.Id, lamport: req.Lamport})
 	} else {
 
 		go func() {
 
 			p.mutex.Lock()
-			p.lamport = max(req.Lamport, p.lamport) + 1
+			//p.lamport = max(req.Lamport, p.lamport) + 1
 			time.Sleep(10 * time.Millisecond)
 			//p.lamport++
 			p.replies[req.Id] = false
 			log.Printf("(%v, %v) Send | Allowing %v to enter critical section\n", p.id, p.lamport, req.Id)
-			p.clients[req.Id].Reply(p.ctx, &message.Info{Id: p.id, State: p.state})
+			p.clients[req.Id].Reply(p.ctx, &message.Info{Id: p.id})
 			p.mutex.Unlock()
 
 		}()
@@ -195,13 +199,13 @@ func (p *peer) Request(ctx context.Context, req *message.Info) (*message.Empty, 
 
 func (p *peer) Reply(ctx context.Context, req *message.Info) (*message.Empty, error) {
 	p.mutex.Lock()
-	p.lamport = max(p.lamport, req.Lamport) + 1
+	//p.lamport = max(p.lamport, req.Lamport) + 1
 	log.Printf("(%v, %v) Recv | Got reply from id %v\n", p.id, p.lamport, req.Id)
 	p.replies[req.Id] = true
 
 	// Added this check in case a the peer responded but that peer itself is in wanted state.
 	// For this case, the responding peer that won access enqueues the request and responds when it is done,
-	// since requests are only issued once the peer that lost will eventually get it when it is released from the queue.
+	//????     since requests are only issued once the peer that lost will eventually get it when it is released from the queue ???    ?
 
 	// if req.State == "WANTED" {
 	// 	p.queue = append(p.queue, msg{id: req.Id, lamport: req.Lamport})
@@ -209,6 +213,7 @@ func (p *peer) Reply(ctx context.Context, req *message.Info) (*message.Empty, er
 
 	if allTrue(p.replies) {
 		p.state = "HELD"
+		p.last_Req = math.MaxInt32
 		for peer := range p.replies {
 			p.replies[peer] = false
 		}
@@ -231,8 +236,9 @@ func allTrue(m map[int32]bool) bool {
 
 func (p *peer) enter() {
 	p.lamport++
+	p.last_Req = p.lamport
 	log.Printf("(%v, %v) Send | Seeking critical section access", p.id, p.lamport)
-	info := &message.Info{Id: p.id, Lamport: p.lamport, State: p.state}
+	info := &message.Info{Id: p.id, Lamport: p.lamport}
 	for id, client := range p.clients {
 
 		_, err := client.Request(p.ctx, info)
@@ -250,7 +256,7 @@ func max(a int32, b int32) int32 {
 	}
 }
 
-func writeToFile(logFile *os.File, id int32, lamport int32, message string) {
+func accessRessource(logFile *os.File, id int32, lamport int32, message string) {
 	timestamp := time.Now().Format("2006-01-02 15:04:05")
 	_, err := fmt.Fprintf(logFile, "[%s] (Lamport: %d) %v %s\n", timestamp, lamport, id, message)
 	if err != nil {
@@ -259,22 +265,21 @@ func writeToFile(logFile *os.File, id int32, lamport int32, message string) {
 }
 
 func setLog(port int32) (*os.File, *os.File) {
-	// Clears the log.txt file when a new server is started
+
 	filename := fmt.Sprintf("logs/peer(%v)-log.txt", port)
 	if err := os.Truncate(filename, 0); err != nil {
 		log.Printf("Failed to truncate: %v\n", err)
 	}
-
-	// This connects to the log file/changes the output of the log informaiton to the log.txt file.
+	//peer
 	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("Error opening file: %v", err)
 	}
-	// print to both file and console
 	mw := io.MultiWriter(os.Stdout, f)
 	log.SetFlags(0)
 	log.SetOutput(mw)
 
+	//cs-log
 	cslog, err := os.OpenFile("logs/cs-log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND|os.O_TRUNC, 0666)
 	if err != nil {
 		log.Fatalf("Error opening critical section log file: %v", err)
